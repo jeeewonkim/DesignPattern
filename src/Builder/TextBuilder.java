cca228acf88a6303492ed0b537b2d7fac8f5eb91
jeon3 <wldnjs3865@naver.com>
2023-04-23 01:23:45 +0900

    feat: 7주차 builder

---
+++ src/Builder/TextBuilder.java
@@ -0,0 +1,38 @@
+package Builder;
+
+public class TextBuilder extends Builder{
+    private StringBuilder sb = new StringBuilder();
+    @Override
+    public void makeTitle(String title) {
+        sb.append("=============================\n");
+        sb.append("[");
+        sb.append(title);
+        sb.append("]\n\n");
+    }
+
+    @Override
+    public void makeString(String string) {
+        sb.append("★");
+        sb.append(string);
+        sb.append("\n\n");
+    }
+
+    @Override
+    public void makeItems(String[] items) {
+        for(String s : items){
+            sb.append("  -");
+            sb.append(s);
+            sb.append("\n");
+        }
+        sb.append("\n");
+    }
+
+    @Override
+    public void close() {
+        sb.append("=============================\n");
+    }
+
+    public String getTextResult(){
+        return sb.toString();
+    }
+}
